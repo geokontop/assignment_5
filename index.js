5914260f6597ef5938bfd3ca6e4fc8fac5d352e2
geokontop <geokontop1960@gmail.com>
2018-11-07 19:25:31 +0200

    first ann

--- index.js
+++ index.js
@@ -28,27 +28,4 @@ if(require.main === module){
 // Export app
 module.exports = app;
 
-// const helpers = require('./app/helpers')
-// const ann = require('./app/lib')
 
-
-// try {
-//     console.log(JSON.stringify(ann.initializeNetwork([3,5.9,4,2])))
-//   }
-//   catch(e) {
-//     console.log(e);
-//     // expected output: "Parameter is not a number!"
-//   }
-
-// const state = [
-//     { 'weights':[[.5,.4],[.9,1]], 'threshold':[.8,-.1]},
-//     { 'weights':[[-1.2,1.1]], 'threshold':[.3]}
-// ]
-
-// const out = ann.activate(state,[1,1],[0]); //out  [ [ 1, 1 ], [ 0.525, 0.8808 ], [ 0.5097 ] ]
-
-// console.log('out ', out)
-
-//  const ot = ann.backPropagate(state,[ [ 1, 1 ], [ 0.525, 0.8808 ], [ 0.5097 ] ],[-0.5097],0.1); //out  [ [ 1, 1 ], [ 0.525, 0.8808 ], [ 0.5097 ] ]
-
-//  console.log('ot ', JSON.stringify(ot))
